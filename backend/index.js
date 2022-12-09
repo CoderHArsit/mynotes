0efaaea6b13069632a9ccd24900501e5920d6ef0
CoderHArsit <hs8449865663@gmail.com>
2022-12-09 13:32:09 +0530

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello harry')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello harry')
+// })
+//Available Routes
+app.use('/api/Auth',require('./Routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
